e76715d41dac7c99e7396147156457a7a561f8ea
Andrew Lee <me@gilgameshskytrooper.io>
2018-11-14 14:19:57 -0600

    updates after review

--- test-cases/createUserTokenTestCases.js
+++ test-cases/createUserTokenTestCases.js
@@ -5,7 +5,7 @@ const createUserTestCases = [
 {
   expectedRc: responseCode.SUCCESS,
   expectedSc: 201,
-  message: 'Successfully created userToken for usr_([a-z0-9]){32}'
+  message: 'Successfully created userToken for usr_([a-z0-9]){32}',
   userToken: 'utk_([a-z0-9]){32}_([0-9]){13}'
 }
 ];
