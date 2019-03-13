4190413e3dc9183bf2fc7b77876b45fb7b52a093
gilgameshskytrooper <me@gilgameshskytrooper.io>
2019-03-13 16:35:50 -0500

    update release.sh

--- release.sh
+++ release.sh
@@ -80,6 +80,27 @@ then
             }
         ]
     }' 'https://hooks.slack.com/services/'$SLACKPARAM1'/'$SLACKPARAM2'/'$SLACKPARAM3
+
+
+      #EMAIL
+      # Just the git commit message title
+      title=$(git log -1 --pretty=%B | head -n 1)
+      git checkout master
+      # Save the messages into an array called message
+      IFS=$'\n' message=($(git log -1 --pretty=%B | sed -e '1,2d'))
+
+      if [[ $title = *"SENDEMAIL"* ]];
+      then
+        formattedmessages=''
+        for i in "${message[@]}"
+        do
+          formattedmessages=$formattedmessages'|'$i
+        done
+
+        json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'", "packagemanaged": "true", "instructions": "npm update"}'
+        curl -X POST -H "Content-Type: application/json" -d $json "https://api.voiceit.io/platform/31"
+      fi
+
     exit 0
   else
     curl -X POST -H 'Content-type: application/json' --data '{
