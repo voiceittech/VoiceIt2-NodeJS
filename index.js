19cdb3472bf575ae3bd202ae43340ad8e6d4b9eb
gilgameshskytrooper <me@gilgameshskytrooper.io>
2019-09-17 11:48:31 -0500

    minor patch to createUserToken() RELEASEPATCH

--- index.js
+++ index.js
@@ -555,7 +555,7 @@ function VoiceIt2(apk, tok, baseUrl) {
       callback({ status: 400, responseCode: 'FAIL', message: 'Missing userId argument' });
     }
 
-    if (options.secondsToTimeout.isNaN()) {
+    if (options.secondsToTimeout !== undefined && options.secondsToTimeout.isNaN()) {
       callback({ status: 400, responseCode: 'FAIL', message: 'secondsToTimeout must be a numeric value' });
     }
 
